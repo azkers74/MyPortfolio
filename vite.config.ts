337693c6b07ef291feae6fe554a19e88c6613a4b
gs.gpsprojectstaff1 <gs.gpsprojectstaff1@gmail.com>
2026-04-27 09:31:57 +0800

    MyPortfolio V.3

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/MyPortfolio/",
+  base: './',
+
+  server: {
+    host: "::",
+    port: 3000,
+  },
+
   plugins: [react()],
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    assetsInlineLimit: 500000,
+    rollupOptions: {
+      output: {
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+  },
 })
+
+
+
